de490ae6c821f3f274a04e2aeb4e02c38204bc0e
ユウぶん <fuziikogei@gmail.com>
2026-05-11 17:54:49 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'diatonic-maker-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png',
+  'https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Fira+Code:wght@300;400;500&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
